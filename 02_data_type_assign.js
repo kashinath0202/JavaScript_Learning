19fe8eb6ec5efb6425f9ba71c81620cf36419adb
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-10-10 09:43:08 -0700

    Data Type
    Data Type

---
+++ 02_data_type_assign.js
@@ -0,0 +1,25 @@
+//Question No.01  (Type Of)
+console.warn("Data Type Of:-","string","number","boolean");
+
+var intro="kashi";
+var birtgday = 02+"july"+1997;
+var moboileNumber=9096949202;
+var marrirdStatus=true;
+                   //undefined
+
+
+console.log("Name-kashi Type:-",typeof(intro));
+console.log("Date Of Birth -02+july+1997 Type:-",typeof(birtgday));
+console.log("Mobile Number-9096949202 Type:-",typeof(moboileNumber));
+console.log("Married Status-true Type:-",typeof(marrirdStatus));
+
+//Question No.02  (Type Of)
+console.warn("Difference Between Null VS Undefined  ");
+
+var hello=  
+
+console.log(hello);
+
+var word=null;
+
+console.log(word);
