def05fa46fd489f3d4eaab56b51cc484dce47899
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-11-24 22:31:24 +0530

    String Assign
    String Assign

---
+++ 04_string_assign.js
@@ -0,0 +1,20 @@
+console.log("================================Question No-1================================");
+console.log("############### My Dream Company Name ###############");
+    
+var like=`My dream company is "Oracle"`;
+console.log(like);
+
+console.log("================================Question No-2================================");
+console.log("############### My Hobbies are ###############");
+
+var hobbies_1='Reading Books';
+var hobbies_2='Playing Cricket';
+var hobbies_3='listening Music';
+
+console.log("1.",hobbies_1);
+console.log("2.",hobbies_2);
+console.log("2.",hobbies_3);
+
+console.log("############### Concat String in One Line ###############");
+
+console.log(hobbies_1.concat(hobbies_2,hobbies_3));
