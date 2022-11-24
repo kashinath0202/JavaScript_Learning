5e37133b0df440de37fbedb3a6294ce8250de814
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-11-24 22:30:25 +0530

    Ternary Operator
    Ternary Operator

---
+++ 05_ternary_ope_assign_05.js
@@ -0,0 +1,18 @@
+console.warn("======================= Q No.01=======================");
+
+console.log(" 1.0==`` Result is : ",0==``);
+console.log(" 2.0==`0` Result is : ",0==`0`);
+console.log(" 3.0==false Result is : ",0==false);
+console.log(" 4.null==undefined Result is : ",null==undefined);
+console.log(" 5.1==[1] Result is : ",1==[1]);
+console.log(" 6.1==true` Result is : ",1==true);
+console.log(" 7.0==1=='1'` Result is : ",0==1=='1');
+
+console.warn("======================= Q No.02=======================");
+
+var greatest=function(num1,num2,num3){
+
+    var result_1= num1>=num2
+ 
+}
+var result=greatest(100,101,90);
