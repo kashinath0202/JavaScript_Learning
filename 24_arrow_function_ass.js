5ed1664c7bf73020ecc4e31d30063ee161824954
Kashinath Gopal Jeure <kashinathjeure@gmail.com>
2022-11-26 20:15:58 +0530

    Arrow Function
    Arrow Function

---
+++ 24_arrow_function_ass.js
@@ -0,0 +1,26 @@
+console.warn("==================== Arrow Function ====================");
+
+let greet=()=>{
+console.log(1. ,"Good Morning, Today is Friday");
+}
+greet();
+
+console.warn("==================== 2.Multiplication of  5*5*2  & 10,4,8 = ====================");
+
+let multiplication_function=(Number1,Number2,Number3=3)=>{
+
+    console.log(Number1*Number2*Number3);
+}
+multiplication_function(5,5,2)
+multiplication_function(10,4,)
+
+
+let addition=(num1,num2,num3,num4,num5,)=>{
+
+     return num1+num2+num3+num4+num5
+}
+console.warn("==================== 3.a - Addition of  100+100+200+349+756  ====================");
+  console.log(addition(100,100,200,349,756));
+ console.warn("==================== 3.c - Invoke same =>  I am,learning,ES6,features,in depth  ====================");
+ console.log(addition("I am","learning","ES6","features","in depth")); 
+                
